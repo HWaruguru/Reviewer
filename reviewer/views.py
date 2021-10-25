81192b3fb0c8b9d56a78475e9b9f6c210cb005d9
HWaruguru <hannah.waruguru8@gmasil.com>
2021-10-25 21:53:20 +0300

    add a view for index

--- reviewer/views.py
+++ reviewer/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
 
 def index(request):
-    return render(request, 'index.html')
+    projects = [{"image":"lamb.jpeg","title": "github", "description": "an angular app", "link": "https://github.com/HWaruguru/Photo-Album/blob/main/photoalbum/templates/index.html","posted_by":"Hannah","date": "21/102021"}, {"image":"lamb.jpeg","title": "github", "description": "an angular app", "link": "https://github.com/HWaruguru/Photo-Album/blob/main/photoalbum/templates/index.html","posted_by":"Hannah","date": "21/102021"}, {"image":"lamb.jpeg","title": "github", "description": "an angular app", "link": "https://github.com/HWaruguru/Photo-Album/blob/main/photoalbum/templates/index.html","posted_by":"Hannah","date": "21/102021"}]
+    return render(request, 'index.html', {"projects":projects})

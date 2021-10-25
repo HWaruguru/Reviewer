0e9de2d3edb6116823835290384e5532943f9358
HWaruguru <hannah.waruguru8@gmasil.com>
2021-10-25 23:42:26 +0300

    add an admin url

--- reviewer/urls.py
+++ reviewer/urls.py
@@ -1,9 +1,12 @@
-from django.conf.urls import url
-from . import views
+from django.conf.urls import include, url
+from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns=[
+    url(r'^admin/', admin.site.urls),
     url(r'^$',views.index,name='index'),
 ]
 

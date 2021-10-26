411e42b7786ceb8b507ce09275afd1d281f10553
HWaruguru <hannah.waruguru8@gmasil.com>
2021-10-26 14:55:41 +0300

    add profile and rating models

--- reviewer/models.py
+++ reviewer/models.py
@@ -9,8 +9,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     profile_picture = models.ImageField(upload_to='images/', default='default.png')
     bio = models.TextField(max_length=500, default="My Bio", blank=True)
-    name = models.CharField(blank=True, max_length=120)
-    location = models.CharField(max_length=60, blank=True)
     contact = models.EmailField(max_length=100, blank=True)
 
     def __str__(self):
@@ -25,20 +23,27 @@ class Profile(models.Model):
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
 
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
 
 class Project(models.Model):
     title = models.CharField(max_length=155)
     url = models.URLField(max_length=255)
     description = models.TextField(max_length=255)
-    technologies = models.CharField(max_length=200, blank=True)
     photo = models.ImageField(upload_to='projects/', default='default.png')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    date = models.DateTimeField(auto_now_add=True, blank=True)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects")
+    
     def __str__(self):
         return f'{self.title}'
 
-    def delete_post(self):
+    def delete_project(self):
         self.delete()
 
     @classmethod
@@ -46,10 +51,10 @@ class Project(models.Model):
         return cls.objects.filter(title__icontains=title).all()
 
     @classmethod
-    def all_posts(cls):
+    def all_projects(cls):
         return cls.objects.all()
 
-    def save_post(self):
+    def save_project(self):
         self.save()
 
 

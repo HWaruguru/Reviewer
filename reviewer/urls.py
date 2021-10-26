b0e84d44da46378c07013a5687af4182aa10700d
HWaruguru <hannah.waruguru8@gmasil.com>
2021-10-26 14:56:55 +0300

    add login and logout urls

--- reviewer/urls.py
+++ reviewer/urls.py
@@ -8,6 +8,10 @@ from . import views
 urlpatterns=[
     url(r'^admin/', admin.site.urls),
     url(r'^$',views.index,name='index'),
+     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url( r'^login/$',auth_views.LoginView.as_view(template_name="useraccounts/login.html"), name="login"),
+    url(r'^logout/$', auth_views.logout, {"next_page": '/'}), 
+    url(r'^signup/$', views.signup, name='signup'),
 ]
 
 if settings.DEBUG:
